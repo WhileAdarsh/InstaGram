82c78e678ed5198c0e479024d9919b6d923aa6bc
WhileAdarsh <its.adarshcd@gmail.com>
2025-07-06 17:26:44 +0530

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
 
-function Home() {
+export default function Home() {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <div className="home">
+      <div className="card">
+        {/* Card header */}
+        <div className="card-header">
+          <div className="card-pic">
+            <img
+              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D"
+              alt="profile"
+            />
+          </div>
+          <h5>Ramesh</h5>
+        </div>
 
-export default Home
+        {/* Uploaded image */}
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1593642632823-8f785ba67e45" alt="uploaded" />
+
+        </div>
+
+        {/* Card content */}
+        <div className="card-content">
+          <span className="material-symbols-outlined">favorite</span>
+          <p>1 Like</p>
+          <p>This is amazing</p>
+        </div>
+
+        {/* Add comment */}
+        <div className="add-comment">
+          <span className="material-symbols-outlined">mood</span>
+          <input type="text" placeholder="Add a comment" />
+          <button className="comment">Post</button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from 'react';
+import './Profile.css'; // make sure you have your CSS imported
 
 function Profile() {
   return (
-    <div>
-      profile
+    <div className="profile">
+      {/* Profile header */}
+      <div className="profile-frame">
+        <div className="profile-pic">
+          <img
+              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D"
+            alt="Profile"
+          />
+        </div>
+        <div className="profile-data">
+          <h1>Canta Coder</h1>
+          <div className="profile-info" style={{ display: "flex" }}>
+            <p>40 posts</p>
+            <p>40 followers</p>
+            <p>40 following</p>
+          </div>
+          <button className="followBtn">Follow</button>
+        </div>
+      </div>
+
+      {/* Gallery */}
+      <div className="gallery">
+        <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D" alt="uploaded" />
+        <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D" alt="uploaded" />
+        <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D" alt="uploaded" />
+        <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D" alt="uploaded" />
+        
+      </div>
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
