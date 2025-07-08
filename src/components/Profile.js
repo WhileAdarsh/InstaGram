381191298412944474fc7190570bf34d040afd53
WhileAdarsh <its.adarshcd@gmail.com>
2025-07-08 12:51:27 +0530

    commit

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -13,7 +13,7 @@ function Profile() {
           />
         </div>
         <div className="profile-data">
-          <h1>Canta Coder</h1>
+          <h1>Adarsh Kumar</h1>
           <div className="profile-info" style={{ display: "flex" }}>
             <p>40 posts</p>
             <p>40 followers</p>
@@ -29,7 +29,7 @@ function Profile() {
         <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D" alt="uploaded" />
         <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D" alt="uploaded" />
         <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D" alt="uploaded" />
-        
+
       </div>
     </div>
   );
